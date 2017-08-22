adcdd392f4659892a7ff0d4c3005dd0bc233d298
rayoelperro <rayoelperro@gmail.com>
2017-08-22 15:01:34 +0200

    Navem run

---
+++ Navem run/Program.cs
@@ -0,0 +1,12 @@
+﻿using Navem_engine;
+
+namespace Navem_run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new NavemWin(args);
+        }
+    }
+}
---
+++ Navem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Navem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args[0])
+            {
+                case "client":
+                    Process.Start("Navem client.exe");
+                    break;
+
+                case "run":
+                    Process.Start("Navem run.exe");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
